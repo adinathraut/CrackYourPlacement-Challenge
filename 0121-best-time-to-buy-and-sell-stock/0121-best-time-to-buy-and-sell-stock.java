d1276c011c66fac65438d0e76d51331d4dd3ee7f
Adinath Raut <82745544+adinathraut@users.noreply.github.com>
2024-07-19 00:23:50 +0530

    Update 0121-best-time-to-buy-and-sell-stock.java

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,14 @@
 class Solution {
-  public int maxProfit(int[] prices) {
-    int sellOne = 0;
-    int holdOne = Integer.MIN_VALUE;
-
-    for (final int price : prices) {
-      sellOne = Math.max(sellOne, holdOne + price);
-      holdOne = Math.max(holdOne, -price);
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
     }
-
-    return sellOne;
-  }
 }
