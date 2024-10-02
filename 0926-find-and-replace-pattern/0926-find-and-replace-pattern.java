341005ecffa1edf2efe994c0786be50816ae9cf8
Adinath Raut <82745544+adinathraut@users.noreply.github.com>
2024-10-02 11:53:00 +0530

    Time: 0 ms (100%), Space: 41.6 MB (99.38%) - LeetHub

--- 0926-find-and-replace-pattern/0926-find-and-replace-pattern.java
+++ 0926-find-and-replace-pattern/0926-find-and-replace-pattern.java
@@ -1,42 +1,16 @@
-// import java.util.*;
-
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
-        List<String> ans = new ArrayList<>();
-        
+        List<String> res = new ArrayList<>();
         for (String word : words) {
-            if (matchesPattern(word, pattern)) {
-                ans.add(word);
-            }
+            if (check(word, pattern)) res.add(word);
         }
-        
-        return ans;
+        return res;
     }
     
-    private boolean matchesPattern(String word, String pattern) {
-        if (word.length() != pattern.length()) return false;
-        
-        // Map to store character mappings
-        Map<Character, Character> map1 = new HashMap<>();
-        Map<Character, Character> map2 = new HashMap<>();
-        
-        for (int i = 0; i < word.length(); i++) {
-            char w = word.charAt(i);
-            char p = pattern.charAt(i);
-            
-            // Check if the mapping exists and if it is consistent
-            if (map1.containsKey(w) && map1.get(w) != p) {
-                return false;
-            }
-            if (map2.containsKey(p) && map2.get(p) != w) {
-                return false;
-            }
-            
-            // Add the mapping
-            map1.put(w, p);
-            map2.put(p, w);
+    boolean check(String a, String b) {
+        for (int i = 0; i < a.length(); i++) {
+            if (a.indexOf(a.charAt(i)) != b.indexOf(b.charAt(i))) return false;
         }
-        
         return true;
     }
 }
