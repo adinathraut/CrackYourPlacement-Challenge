7a4f4ae821a16ac5602ce8265375fa1a423e8080
Adinath Raut <82745544+adinathraut@users.noreply.github.com>
2024-11-13 00:04:43 +0530

    Time: 1 ms (93.57%), Space: 44.6 MB (74.3%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,19 +1,34 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int pro=Integer.MIN_VALUE;
-        int pref=1;
-        int suff=1;
-        int n=nums.length;
+        // int pro=Integer.MIN_VALUE;
+        // int pref=1;
+        // int suff=1;
+        // int n=nums.length;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(suff==0) suff=1;
+        //     if(pref==0) pref=1;
+        //     suff*=nums[i];
+        //     pref*=nums[n-i-1];
+        //     pro=Math.max(pro,Math.max(suff,pref));
+        //    // int cur=proe*nums[i];
+        // }
+        // return pro;
+        int pro=1;
+        int mx=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++)
-        {
-            if(suff==0) suff=1;
-            if(pref==0) pref=1;
-            suff*=nums[i];
-            pref*=nums[n-i-1];
-            pro=Math.max(pro,Math.max(suff,pref));
-
-           // int cur=proe*nums[i];
+        {  
+            pro*=nums[i];
+            mx=Math.max(mx,pro);
+          if(pro==0) pro=1;
         }
-        return pro;
+            pro=1;
+        for(int i=nums.length-1;i>=0;i--)
+        {  
+            pro*=nums[i];
+            mx=Math.max(mx,pro);
+          if(pro==0) pro=1;
+        }
+        return mx;
     }
 }
